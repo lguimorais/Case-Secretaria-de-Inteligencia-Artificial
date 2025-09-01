ea59fb9a0a591a26880335346874ac3d50f22cff
luis guilherme <163307645+lguimorais@users.noreply.github.com>
2025-09-01 18:11:07 -0300

    alteraçao da url base para a url correta onde encontra dados q eu desejo

--- requisicao.py
+++ requisicao.py
@@ -1,36 +1,39 @@
-import requests #biblioteca de requisições
-import xml.etree.ElementTree as ET #bibliotaca de processar XML
+import requests  # biblioteca de requisições
+import xml.etree.ElementTree as ET  # bibliotaca de processar XML
 import re
+# termos q vou utilizar como metodo de pesquissa
+termos_de_pesquisa = ["Inteligência Artificial Piauí"," SIA Piauí"]
 
-#termos q vou utilizar como metodo de pesquissa
-termos_de_pesquisa = ["Iteligência Artificial Piaui", "Governo do estado do piaui","rafael fonteles e inteligencia artificial"]
+# termos_de_pesquisa = ["Inteligência Artificial Piauí OR SIA Piauí"]
 
-#url base usado para pegar os conteudos
-base_url = "https://news.google.com/rss/search?q={}"
-
-#numero de noticias q serão pesquisadas
+# url base usado para pegar os conteudos
+base_url = "https://news.google.com/rss/search?q={}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
+# numero de noticias q serão pesquisadas
 Num_noticias = 15
 
 TAG_RE = re.compile(r'<[^>]+>')
 
+
 for termo in termos_de_pesquisa:
-  termo_formatado = termo.replace(" ","%20")
-  url_completa = base_url.format(termo_formatado)
-  
-  response = requests.get(url_completa)
-  
-  root = ET.fromstring(response.text)
-  itens = root.findall('channel/item')[:Num_noticias]
+    termo_formatado = termo.replace(" ", "%20")
+    url_completa = base_url.format(termo_formatado)
+    response = requests.get(url_completa)
+    root = ET.fromstring(response.text)
+    itens = root.findall('channel/item')
 
+    # if not itens:
+    #     print("⚠ Nenhuma notícia encontrada.")
+    #     continue
 
+    for item in itens[:Num_noticias]:
+        titulo = item.find('title').text
+        link = item.find('link').text
+        descricao_bruta = item.find('description').text or " "
 
-for item in itens:
-  titulo = item.find('title').text
-  link = item.find('link').text
-  descricao_bruta = item.find('description').text
-  descricao_limpa = TAG_RE.sub('',descricao_bruta)
-  print("--- Notícia ---")
-  print(f"Título: {titulo}")
-  print(f"Link: {link}")
-  print(f"Descrição: {descricao_limpa.strip()}")
-  print("-" * 20)
+        descricao_limpa = TAG_RE.sub('', descricao_bruta)
+
+        print("--- Notícia ---")
+        print(f"Título: {titulo}")
+        print(f"Link: {link}")
+        print(f"Descrição: {descricao_limpa.strip()}")
+        print("-" * 20)
