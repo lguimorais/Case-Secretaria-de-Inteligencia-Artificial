0f3353e1be553012ec3f6f223925bafa245fe27c
luis guilherme <163307645+lguimorais@users.noreply.github.com>
2025-09-01 21:14:02 -0300

    instalando da forma correta a biblioteca pandas

--- requisicao.py
+++ requisicao.py
@@ -1,6 +1,7 @@
 import requests  # biblioteca de requisições
 import xml.etree.ElementTree as ET  # bibliotaca de processar XML
 import re
+import pandas as pd
 # termos q vou utilizar como metodo de pesquissa
 termos_de_pesquisa = ["Inteligência Artificial Piauí", " SIA Piauí"]
 
@@ -8,9 +9,16 @@ termos_de_pesquisa = ["Inteligência Artificial Piauí", " SIA Piauí"]
 # url base usado para pegar os conteudos
 base_url = "https://news.google.com/rss/search?q={}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
 # numero de noticias q serão pesquisadas
-Num_noticias = 15
-
+Num_noticias = 20
+palavras_positivas = ["crescimento", "sucesso", "inovação",
+                      "avanços", "investimento", "parceria", "melhoria", "expansão"]
+palavras_negativas = ["desafio", "crise", "problema",
+                      "falha", "atraso", "dificuldade", "incerto", "ameaça"]
 TAG_RE = re.compile(r'<[^>]+>')
+
+
+def classificacao_de_qualidade(texto):
+    return
 
 
 for termo in termos_de_pesquisa:
@@ -27,14 +35,13 @@ for termo in termos_de_pesquisa:
     for item in itens[:Num_noticias]:
         titulo = item.find('title').text
         link = item.find('link').text
-        descricao_bruta = item.find('description').text or " "
-
+        descricao_bruta = item.find(
+            'description').text.replace("&nbsp;&nbsp;", " - ") or " "
         descricao_limpa = TAG_RE.sub('', descricao_bruta)
 
-
-        # prints utilizados para verificar quais dados estavam vindo para melhor desempenho do codio
-        # print("--- Notícia ---")
-        # print(f"Título: {titulo}")
-        # print(f"Link: {link}")
-        # print(f"Descrição: {descricao_limpa.strip()}")
-        # print("-" * 20)
+       # prints utilizados para verificar quais dados estavam vindo para melhor desempenho do codio
+        print("--- Notícia ---")
+        print(f"Título: {titulo}")
+        print(f"Link: {link}")
+        print(f"Descrição: {descricao_limpa.strip()}")
+        print("-" * 20)
