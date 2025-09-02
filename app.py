a89e09eb42a4eb2f5ceced07ac6d8162d333134c
luis guilherme <163307645+lguimorais@users.noreply.github.com>
2025-09-02 10:35:27 -0300

    documentaçao do codigo

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ dados_noticias = pd.read_json('noticias.json')
 titulo = st.title('Monitoramento de Notícias sobre IA no Piauí')
 tabela = st.dataframe(dados_noticias)
 sentimentos = dados_noticias['Sentimento'].value_counts()
-figura , eixo_do_grafico = plt.subplots()
+figura, eixo_do_grafico = plt.subplots()
 
 eixo_do_grafico.pie(sentimentos, labels=sentimentos.index,
-                     autopct='%1.1f%%', startangle=90)
+                    autopct='%1.1f%%', startangle=90)
 eixo_do_grafico.set_title('Distribuição de Sentimento das Notícias')
 # Garante que o círculo seja desenhado corretamente
 eixo_do_grafico.axis('equal')
