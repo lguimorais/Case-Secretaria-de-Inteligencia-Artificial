cb53e81f8eb6f6c710b05646ea9a77ad8e2b1e9c
luis guilherme <163307645+lguimorais@users.noreply.github.com>
2025-09-01 22:38:42 -0300

    implementaçao da geracao do arquivo.json

--- requisicao.py
+++ requisicao.py
@@ -2,23 +2,54 @@ import requests  # biblioteca de requisições
 import xml.etree.ElementTree as ET  # bibliotaca de processar XML
 import re
 import pandas as pd
+
 # termos q vou utilizar como metodo de pesquissa
-termos_de_pesquisa = ["Inteligência Artificial Piauí", " SIA Piauí"]
+termos_de_pesquisa = [
+    "Inteligência Artificial Piauí", " SIA Piauí", "IA Piauí"]
 
 
 # url base usado para pegar os conteudos
 base_url = "https://news.google.com/rss/search?q={}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
 # numero de noticias q serão pesquisadas
-Num_noticias = 20
-palavras_positivas = ["crescimento", "sucesso", "inovação",
-                      "avanços", "investimento", "parceria", "melhoria", "expansão"]
-palavras_negativas = ["desafio", "crise", "problema",
-                      "falha", "atraso", "dificuldade", "incerto", "ameaça"]
+Num_noticias = 50
+palavras_positivas = [
+    "avanço", "inovação", "modernização", "referência", "pioneirismo",
+    "oportunidades", "gratuito", "qualificação", "capacitação", "parceria",
+    "fortalecimento", "protagonismo", "transformação", "revolução", "inclusão",
+    "integração", "proteção", "liderança", "sucesso", "premiação",
+    "cooperação", "destaque", "desenvolvimento", "expansão", "educação",
+    "tecnologia", "sustentabilidade", "agilidade"
+]
+
+palavras_negativas = [
+    "golpe", "prisão", "suspeito", "matar", "homicídio", "urgente",
+    "criminalidade", "irregularidades", "endividamento", "risco",
+    "problema", "pressão", "desafio", "conflito", "dependência"
+]
+# lista onda ficara todas as noticias
+todas_as_noticias = []
+
+
 TAG_RE = re.compile(r'<[^>]+>')
 
 
-def classificacao_de_qualidade(texto):
-    return
+def classificacao_do_texto(texto):
+    avaliacao_positiva, avaliacao_negativa = 0, 0
+    texto_minusculo = texto.lower()
+
+    for palavra in palavras_positivas:
+        if palavra in texto_minusculo:
+            avaliacao_positiva += 1
+    for palavra in palavras_negativas:
+        if palavra in texto_minusculo:
+            avaliacao_negativa += 1
+
+    if avaliacao_positiva > avaliacao_negativa:
+        return "Positivo"
+    elif avaliacao_positiva < avaliacao_negativa:
+        return "Negativo"
+    else:
+        return "Neutro"
 
 
 for termo in termos_de_pesquisa:
@@ -38,10 +69,20 @@ for termo in termos_de_pesquisa:
         descricao_bruta = item.find(
             'description').text.replace("&nbsp;&nbsp;", " - ") or " "
         descricao_limpa = TAG_RE.sub('', descricao_bruta)
-
+        texto_completo = titulo + " " + descricao_limpa
+        sentimento = classificacao_do_texto(texto_completo)
+        noticia_dict = {
+            'Titulo': titulo,
+            'Link': link,
+            'Descricao': descricao_limpa.strip(),
+            'Sentimento': sentimento
+        }
+        todas_as_noticias.append(noticia_dict)
+dataframe = pd.DataFrame(todas_as_noticias)
+dataframe.to_json('noticias.json')
        # prints utilizados para verificar quais dados estavam vindo para melhor desempenho do codio
-        print("--- Notícia ---")
-        print(f"Título: {titulo}")
-        print(f"Link: {link}")
-        print(f"Descrição: {descricao_limpa.strip()}")
-        print("-" * 20)
+        # print("--- Notícia ---")
+        # print(f"Título: {titulo}")
+        # # print(f"Link: {link}")
+        # print(f"Descrição: {descricao_limpa.strip()}")
+        # print("-" * 20)
